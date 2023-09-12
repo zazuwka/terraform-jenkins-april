945b80b858040ce91b9ea6742524615c9b954f00
zazuwka <131808561+zazuwka@users.noreply.github.com>
2023-09-11 20:29:12 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,9 @@
 provider "aws" {
 region = "us-east-2"
 }
-
-
 resource "aws_instance" "web" {
-ami = "ami-092b51d9008adea15"
-instance_type = "t2.micro"
-availability_zone = "us-east-2a"
-key_name = "my-laptop-key"
+ami = var.ami_id
+instance_type = var.instance_type
+availability_zone = var.az1
+key_name = var.key_name
 }
