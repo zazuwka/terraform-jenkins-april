88703184f15352d96947bd83fd8a4df648cb52a1
zazuwka <131808561+zazuwka@users.noreply.github.com>
2023-09-11 20:30:04 -0500

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,19 @@
+variable ami_id {
+    type = string
+    default = ""
+}
+
+variable instance_type {
+    type = string
+    default = ""
+}
+
+variable az1 {
+    type = string
+    default = ""
+}
+
+variable key_name {
+    type = string
+    default = ""
+}
