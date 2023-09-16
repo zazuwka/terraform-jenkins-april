41beda924499fa598b20620d2a1e662334656920
zazuwka <131808561+zazuwka@users.noreply.github.com>
2023-09-16 16:33:55 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,17 @@ provider "aws" {
 region = var.region
 }
 resource "aws_instance" "web" {
-ami = var.ami_id
+ami = var.ami_name
 instance_type = var.instance_type
 availability_zone = var.az1
 key_name = var.key_name
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = [var.ami_name]
+  }
+  owners = ["self"]
 }
